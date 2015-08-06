3edcca5e79ab09903ada1516b6452e4892994b77
Steve Karsch <steve.karsch@rodale.com>
2015-08-06 13:54:14 -0400

    Add drush to path in post-install.

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,7 @@ cd /home/vagrant
 sudo -u vagrant curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 sudo -u vagrant sed -i '1i export PATH="/home/vagrant/.composer/vendor/bin:$PATH"' /home/vagrant/.bashrc
+echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' > /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 composer global require drush/drush:7.*
 mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%';FLUSH PRIVILEGES;"i
