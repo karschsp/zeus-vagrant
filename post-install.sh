f2b8a527b67e973c61fa5dabe58f56c11071a03c
Steve Karsch <steve.karsch@rodale.com>
2015-08-07 07:19:04 -0400

    Use trusty64 instead of precise. Some changes to post-install.

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,9 @@
 cd /home/vagrant
 sudo -u vagrant curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+sudo mv composer.phar /usr/local/bin/composer
 sudo -u vagrant sed -i '1i export PATH="/home/vagrant/.composer/vendor/bin:$PATH"' /home/vagrant/.bashrc
 echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' > /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 composer global require drush/drush:7.*
-mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%';FLUSH PRIVILEGES;"i
-
+mysql -u root -h 127.0.0.1 -e "GRANT ALL ON *.* TO 'root'@'%';FLUSH PRIVILEGES;"
 
